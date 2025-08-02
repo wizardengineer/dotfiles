bcf920d00240ee8d20728ac7a0ef98dd015c28fd
wizardengineer <juliuswoosebert@gmail.com>
2025-08-02 11:30:02 -0400

    Automatic update via ~/.local/bin/cadd

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -4,7 +4,7 @@
 
 return {
   { "alexghergh/nvim-tmux-navigation" },
-
+  { "sitiom/nvim-numbertoggle" },
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
@@ -1072,4 +1072,19 @@ return {
 
     end
   },
+
+  {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+}
 }
